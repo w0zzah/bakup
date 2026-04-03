e3df422007da11da5dcdf690c4166aca5b4dd127
Conrad Wilson <cwi184@uclive.ac.nz>
2026-04-03 20:33:05 +1300

    Interface 'purchasable' implemented

--- src/main/java/seng201/nunbutblood/models/Purchasable.java
+++ src/main/java/seng201/nunbutblood/models/Purchasable.java
@@ -4,10 +4,25 @@ package seng201.nunbutblood.models;
 Last Edited 0412 02/04/26 @author cwi184 - "Interface for Market interactions"
 
         Purchasable Interface --- Shared by Items and Apostles
-        TODO:
           Methods:
             ~ getCost() : Returns price in Faith
             ~ getDescription() : Returns stats/info for Market display
 */
 public interface Purchasable {
+
+    /*
+    Return the Faith cost of Apostle's and Item's
+
+    @return cost of target
+     */
+    int getCost();
+
+
+    /*
+    Return's the description of Apostle's and Item's
+
+    @return description of target
+     */
+    String getDescription();
+
 }
