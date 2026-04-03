74f0cf17750325800462753b4897d1b131f30360
Conrad Wilson <cwi184@uclive.ac.nz>
2026-04-03 23:19:07 +1300

    Added Market and TODO

---
+++ src/main/java/seng201/nunbutblood/gui/MarketController.java
@@ -0,0 +1,18 @@
+package seng201.nunbutblood.gui;
+
+/*
+        Market Class --- Used for buying/selling apostle's and item's
+        TODO:
+          Methods:
+            ~ refreshStock() : refresh's market
+                ~ Generate Apostle's
+                ~ Generate Item's
+                ~ Multiply stat's or chance based off of Expedition count
+            ~ buyApostle() : Purchase an apostle
+            ~ sellApostle() : Sell Apostle
+            ~ buyItem() : Buy Item
+            ~ sellItem() : Sell Item
+*/
+
+public class MarketController {
+}
