0922bddc1508c63641150c60a0b5921802550301
Conrad Wilson <cwi184@uclive.ac.nz>
2026-04-04 20:07:50 +1300

    Executioner, Constructor and Methods

--- src/main/java/seng201/nunbutblood/characters/Executioner.java
+++ src/main/java/seng201/nunbutblood/characters/Executioner.java
@@ -1,16 +1,73 @@
 package seng201.nunbutblood.characters;
-/*
 
-    Last Edited 0339 02/04/26 @author cwi184 - "Defined Executioner specialization"
+/**
+ * Executioner — Extend's Apostle's, Primary stat is Strength
+ *
+ * When Constructor is called :
+ * @strength has a bonus of +5 clamped at 20 for balance, Math.min(20, strength + 5)
+ * @calcHireCost set's the hire cost, (strength + 5, wisdom, dexterity)
+ * @calcUpKeep set's the upkeep cost, (strength + 5, wisdom, dexterity)
+ *
+ * Constructor to be called in market area for further random stat's
+ */
+public class Executioner extends Apostles {
 
-        Executioner Class --- Extends Apostles
-        TODO:
-          Attributes:
-            ~ strength (int) : Primary stat for dice rolls
-          Methods:
-            ~ addStatToDiceRoll() : Returns strength bonus
-            ~ useAbility() : # Uncertain
-*/
+    public Executioner(String name) {
+        this(name, getPortraitID(name), 8, 5, 5);
+    }
 
-public class Executioner {
+    public Executioner(String name, String portraitID, int strength, int wisdom, int dexterity){
+        super(name, portraitID,
+                Math.min(20, strength + 5),
+                wisdom,
+                dexterity,
+                60,
+                20,
+                calcHireCost(strength + 5, wisdom, dexterity),
+                calcUpKeep(strength + 5, wisdom, dexterity));
+    }
+
+    // APOSTLES CLASS
+
+
+    // PURCHASABLE INTERFACE
+
+    @Override
+    public int getStatBonus() {
+        return getStrength();
+    }
+
+    @Override
+    public String getStatType() {
+        return "STR";
+    }
+
+
+    static String getPortraitID(String name) {
+        return name.toLowerCase().replace(" ", "-");
+    }
+
+    /**
+     *
+     * @param strength
+     * @param wisdom
+     * @param dexterity
+     * @return
+     */
+    static int calcHireCost(int strength, int wisdom, int dexterity) {
+        return 80 + (strength + wisdom + dexterity) * 3;
+    }
+
+    /**
+     * Sum
+     * @param strength
+     * @param wisdom
+     * @param dexterity
+     * @return
+     */
+    static int calcUpKeep(int strength, int wisdom, int dexterity) {
+        return 15 + (strength + wisdom + dexterity) / 5;
+    }
 }
+
+
